3fec3e4e5c64b07e977398c343b095e244843959
Patrick Keroulas <patrick.keroulas@gmail.com>
2019-02-07 14:44:11 -0500

    ui: rename pcap-upload-related time to file-upload
    This allow for a reuse of the upload mechnism for other kinds file.

--- apps/gui/app/components/PcapUploader.js
+++ apps/gui/app/components/PcapUploader.js
@@ -57,44 +57,44 @@ class PcapUploader extends Component {
         }).then(() => {
             this.setState({ uploadComplete: true, isUploading: false, uploadFailed: false });
             notifications.success({
-                title: translate('notifications.success.pcap_upload'),
-                message: translate('notifications.success.pcap_upload_message', { name: fileToUpload.name })
+                title: translate('notifications.success.file_upload'),
+                message: translate('notifications.success.file_upload_message', { name: fileToUpload.name })
             });
         }).catch(() => {
             this.setState({ uploadComplete: false, isUploading: false, uploadFailed: true });
             notifications.error({
-                title: translate('notifications.error.pcap_upload'),
-                message: translate('notifications.error.pcap_upload_message', { name: fileToUpload.name })
+                title: translate('notifications.error.file_upload'),
+                message: translate('notifications.error.file_upload_message', { name: fileToUpload.name })
             });
         });
     }
 
     renderUploadedFiles() {
         return (
-            <div className="lst-pcap-uploader-list fade-in">
+            <div className="lst-file-uploader-list fade-in">
                 <h3>{translate('workflow.pcaps_to_be_imported')}</h3>
-                <ul className="lst-pcap-uploader-files">
+                <ul className="lst-file-uploader-files">
                     {this.state.files.map((file, index) => (
                         <li key={file.name} className="row lst-no-padding">
                             <div className="col-xs-1 middle-xs">
                                 <Icon
-                                    className={classNames('pcap-icon', {
+                                    className={classNames('file-icon', {
                                         'lst-text-red': this.state.uploadFailed
                                     })}
                                     value="insert drive file"
                                 />
                             </div>
                             <div className="col-xs-11">
-                                <div className="pcap-upload-file-info col-xs-12 row">
+                                <div className="file-upload-file-info col-xs-12 row">
                                     <div
-                                        className={classNames('col-xs-10 pcap-file-name', {
+                                        className={classNames('col-xs-10 file-file-name', {
                                             'lst-text-red': this.state.uploadFailed && !this.state.isUploading
                                         })}
                                     >
                                         <span>{file.name}</span>
-                                        <span className="lst-pcap-uploader-file-size">({bytes(file.size)})</span>
+                                        <span className="lst-file-uploader-file-size">({bytes(file.size)})</span>
                                     </div>
-                                    <div className="col-xs-2 pcap-upload-progress">
+                                    <div className="col-xs-2 file-upload-progress">
                                         {this.state.isUploading && `${this.state.uploadProgress[index] ? this.state.uploadProgress[index] : 0}%`}
                                         {this.state.uploadComplete && (
                                             <Icon className="lst-text-green fade-in upload-state-icon" value="check" />
@@ -106,7 +106,7 @@ class PcapUploader extends Component {
                                 </div>
                                 {(this.state.isUploading) && (
                                     <ProgressBar
-                                        className="lst-pcap-uploader-bar fade-in"
+                                        className="lst-file-uploader-bar fade-in"
                                         percentage={this.state.uploadProgress[index] ? this.state.uploadProgress[index] : 0}
                                     />
                                 )}
@@ -132,8 +132,8 @@ class PcapUploader extends Component {
     renderDragAndDropMessage() {
         return (
             <p>
-                {translate('workflow.drop_files_here')} {' '}
-                <span className="lst-pcap-uploader__link">{translate('workflow.select_from_drive')}</span>
+                {translate('workflow.drop_pcap_files_here')} {' '}
+                <span className="lst-file-uploader__link">{translate('workflow.select_from_drive')}</span>
             </p>
         );
     }
@@ -141,17 +141,17 @@ class PcapUploader extends Component {
     renderDropMessage() {
         return (
             <div>
-                <i className="lst-pcap-uploader__icon lst-icons bounce">file_download</i>
-                <p>{translate('workflow.drop_files_here')}</p>
+                <i className="lst-file-uploader__icon lst-icons bounce">file_download</i>
+                <p>{translate('workflow.drop_pcap_files_here')}</p>
             </div>
         );
     }
 
     render() {
-        const dropzoneClassName = classNames(this.props.className, 'lst-pcap-uploader');
+        const dropzoneClassName = classNames(this.props.className, 'lst-file-uploader');
 
         return (
-            <div className="lst-pcap-container">
+            <div className="lst-file-container">
                 <Dropzone
                     className={dropzoneClassName}
                     onDragEnter={() => this.setState({ dragActivated: true })}
